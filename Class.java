e122dc0a113471be0103504bb7f698834f802ccc
GritL1 <bingyanl@nycstudents.net>
2026-04-27 13:42:31 -0400

    Update Class.java

--- Class.java
+++ Class.java
@@ -1,1 +1,18 @@
+public class Class {
+    private int classID;
+    private int courseID;
+    private int period;
+    private String room;
+    private int teacher;
 
+    public Class(int courseID, int period, String room, int teacher){
+        this.courseID = courseID;
+        this.period = period;
+        this.room = room;
+        this.teacher = teacher;
+    }
+
+    public String toString(){
+        return "INSERT INTO Class (courseID, period, room, teacher) VALUES (foreign key, int, varchar(255), foreign key)";
+    }
+}
