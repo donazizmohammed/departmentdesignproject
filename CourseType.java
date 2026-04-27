a2bcbcfa767a4787257c49257f6931cd4a5e638a
GritL1 <bingyanl@nycstudents.net>
2026-04-27 13:34:31 -0400

    Update CourseType.java

--- CourseType.java
+++ CourseType.java
@@ -1,1 +1,13 @@
+public class CourseType {
+    private int typeID;
+    private String typeName;
 
+    public CourseType(String typeName){
+        this.typeName = typeName;
+    }
+
+    @Override
+    public String toString() {
+        return "INSERT INTO CourseType (typeName) VALUES (varchar(255))";
+    }
+}
