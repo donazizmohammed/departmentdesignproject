65bef6ea6a6170234819f4403ef0c2d7528dd3fe
Donaziz Mohammed <donazizm@nycstudents.net>
2026-04-27 13:57:05 -0400

    getters and setters

--- Class.java
+++ Class.java
@@ -5,6 +5,46 @@ public class Class {
     private String room;
     private int teacher;
 
+    public int getClassID() {
+        return this.classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public int getCourseID() {
+        return this.courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public int getPeriod() {
+        return this.period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public String getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public int getTeacher() {
+        return this.teacher;
+    }
+
+    public void setTeacher(int teacher) {
+        this.teacher = teacher;
+    }
+
     public Class(int courseID, int period, String room, int teacher){
         this.courseID = courseID;
         this.period = period;
