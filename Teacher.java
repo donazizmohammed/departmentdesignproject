598e0050039c26d6016b40587ee213770952d195
Bingyan Li <bingyanl@nycstudents.net>
2026-04-29 13:56:08 -0400

    fixes

---
+++ Teacher.java
@@ -0,0 +1,47 @@
+public class Teacher {
+    private int teacherID;
+    private String firstName;
+    private String lastName;
+    private int departmentID;
+    public Teacher(String firstName, String lastName, int departmentID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.departmentID = departmentID;
+    }
+    public String toString(){
+        return "INSERT INTO Teacher ( FirstName, Lastname, DepartmentID) VALUES ( " + firstName + ", " + lastName + ", " + departmentID + ");";
+
+    }
+
+
+
+//    public void teacherInserts() throws FileNotFoundException {
+//        //doesnt work perfectly due to some teachers having middle names but WHO CARES LOL
+//        File input = new File("teachers.txt");
+//
+//        try (Scanner scan = new Scanner(input)) {
+//            while (scan.hasNextLine()) {
+//                String line = scan.nextLine();
+//
+//                String[] parts = line.split("\\|", 2);
+//                String[] name = parts[0].trim().split(" ");
+//
+//                String dept = parts.length > 1 ? parts[1].trim() : "";
+//
+//                String firstName = name[0];
+//                String lastName = (name.length > 1) ? name[1] : "Unknown";
+//
+//                System.out.println("INSERT INTO Teachers (FirstName, LastName, Department) VALUES ('"
+//                        + firstName + "', '" + lastName + "', '" + dept + "');");
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//        try {
+//            Teachers x = new Teachers();
+//            x.teacherInserts();
+//        } catch (FileNotFoundException e) {
+//            System.err.println("File not found" + e.getLocalizedMessage() + "gl");
+//        }
+//    }
+}
