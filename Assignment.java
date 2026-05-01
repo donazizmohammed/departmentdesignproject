53cbac4924195dd27b0ad0edf3fad6e180f4729b
donazizmohammed <donazizm@nycstudents.net>
2026-05-01 13:26:30 -0400

    setters and getters bro setters and getters

--- Assignment.java
+++ Assignment.java
@@ -4,6 +4,31 @@ public class Assignment {
     private int assignmentTypeID;
     private int courseID;
 
+    public String getAssignmentName() {
+        return this.assignmentName;
+    }
+
+    public void setAssignmentName(String assignmentName) {
+        this.assignmentName = assignmentName;
+    }
+
+    public int getAssignmentTypeID() {
+        return this.assignmentTypeID;
+    }
+
+    public void setAssignmentTypeID(int assignmentTypeID) {
+        this.assignmentTypeID = assignmentTypeID;
+    }
+
+    public int getCourseID() {
+        return this.courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+
     public Assignment(String name, int typeID, int cID){
         assignmentName = name;
         assignmentTypeID = typeID;
@@ -12,5 +37,13 @@ public class Assignment {
     }
     public String toString(){
         return "INSERT INTO Assignment ( AssignmentName, AssignmentTypeID, courseID) VALUES ( " + assignmentName + ", " + assignmentTypeID + ", " + courseID + ");";
+    }
+
+    public int getAssignmentID() {
+        return assignmentID;
+    }
+
+    public void setAssignmentID(int assignmentID) {
+        this.assignmentID = assignmentID;
     }
 }
