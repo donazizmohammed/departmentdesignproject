9df205e83e3b0357c9492e611f0170eb48a21fd1
Donaziz Mohammed <donazizm@nycstudents.net>
2026-05-01 13:19:41 -0400

    @Override tsk tsk tsk

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         CourseType elective = new CourseType("Elective");
         CourseType regents = new CourseType("Regents");
         CourseType ap = new CourseType("AP");
@@ -7,6 +13,20 @@ public class Main {
         System.out.println(elective);
         System.out.println(regents);
         System.out.println(ap);
+// attempt at making code that generates student objects from the student.txt file
+// 90% chance this doesnt work but its a start
+        File input = new File("students.txt");
+        try (Scanner scan = new Scanner(input)) {
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] data = line.split(" ");
+                Student student = new Student();
+                student.setFirstName(data[0]);
+                student.setLastName(data[1]);
 
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found" + e.getLocalizedMessage() + "gl");
+        }
     }
 }
