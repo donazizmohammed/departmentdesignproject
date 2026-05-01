085e9f3b5c9bb9b125b919cbfd9e96c10392cde2
BingyanLi <bingyanl@nycstudents.net>
2026-05-01 13:22:56 -0400

    main edits

--- CourseType.java
+++ CourseType.java
@@ -1,14 +1,16 @@
 public class CourseType {
+    private static int count = 0;
     private int typeID;
     private String typeName;
 
     public CourseType(String typeName){
+        this.typeID = count++;
         this.typeName = typeName;
     }
 
     @Override
     public String toString() {
 
-        return "INSERT INTO CourseType (typeName) VALUES (" + typeName + ")";
+        return "INSERT INTO CourseType (typeName) VALUES ( '" + typeName + "')";
     }
 }
