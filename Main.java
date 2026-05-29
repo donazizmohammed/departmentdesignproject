ba64b7019824da56c772c8def3a12d55b1c863a3
BingyanLi <bingyanl@nycstudents.net>
2026-05-29 13:35:51 -0400

    revert

--- Main.java
+++ Main.java
@@ -95,7 +95,7 @@ public class Main {
         System.out.println("DROP TABLE IF EXISTS Students;");
         System.out.println("DROP TABLE IF EXISTS Teacher;");
         System.out.println("DROP TABLE IF EXISTS Courses;");
-        System.out.println("DROP TABLE IF EXISTS Departments;");
+        System.out.println("DROP TABLE IF EXISTS Department;");
         System.out.println("DROP TABLE IF EXISTS CourseType;");
     }
 
