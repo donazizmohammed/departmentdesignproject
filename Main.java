13ee0fc1456219aa6adb5b7440a705eb99b52ec7
BingyanLi <bingyanl@nycstudents.net>
2026-05-29 13:14:00 -0400

    Students2

--- Main.java
+++ Main.java
@@ -92,7 +92,7 @@ public class Main {
         System.out.println("DROP TABLE IF EXISTS Assignment;");
         System.out.println("DROP TABLE IF EXISTS AssignmentType;");
         System.out.println("DROP TABLE IF EXISTS Class;");
-        System.out.println("DROP TABLE IF EXISTS Student;");
+        System.out.println("DROP TABLE IF EXISTS Students;");
         System.out.println("DROP TABLE IF EXISTS Teacher;");
         System.out.println("DROP TABLE IF EXISTS Courses;");
         System.out.println("DROP TABLE IF EXISTS Department;");
